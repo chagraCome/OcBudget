09c78229d24a3146e415e12ff66e0701c5e8c83c
chagraCome <amriasma2016@gmail.com>
2016-05-25 15:40:02 +0100

    probleme d affichage le resultat d un requet findAll

--- src/OC/Bundle/BudgeBundle/Controller/BudgetController.php
+++ src/OC/Bundle/BudgeBundle/Controller/BudgetController.php
@@ -8,8 +8,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class BudgetController extends Controller {
 
     function budgetAction() {
+       
         return $this->render('OCBudgeBundle:Budget:budget.html.twig');
+        
     }
+function showAction() {
+  /*  $em = $this->getDoctrine()->getManager();
+$query = $em->createQuery(
+    'SELECT b
+    FROM OCBundleBudgeBundle:Budget b'
+);
 
+$budgets = $query->getResult();*/
+    $repository = $this
+  ->getDoctrine()
+  ->getManager()
+  ->getRepository('OCBudgeBundle:Line_Budget')
+;
+
+$LineBudgets = $repository->findAll();
+ return $this->render(
+        'OCBudgeBundle:Budget:budget.html.twig',
+        array('LineBudgets' => $LineBudgets)
+    );
+/*foreach ($listLineBudgets as $line) {
+  // $advert est une instance de Advert
+  echo $line->getContent();}*/
+}
 }
 
--- src/OC/Bundle/BudgeBundle/Entity/Line_BudgetRepository.php
+++ src/OC/Bundle/BudgeBundle/Entity/Line_BudgetRepository.php
@@ -12,4 +12,5 @@ use Doctrine\ORM\EntityRepository;
  */
 class Line_BudgetRepository extends EntityRepository
 {
+   
 }
